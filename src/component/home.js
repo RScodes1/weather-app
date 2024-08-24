822b18e15c52907b7ef849ce8e6956e256a61ee9
rajcodes168h <>
2024-08-24 21:30:07 +0530

    full working website

--- src/component/home.js
+++ src/component/home.js
@@ -31,24 +31,24 @@ const checkValidity = (name) =>{
             setError("Please enter a valid city name (letters and spaces only).");
             setShowGif(true);
             setLoading(false);
-            setTimeout(() => setShowGif(false), 3000);
+            setTimeout(() => setShowGif(false), 5250);
             return;
         }
 
         try {
             // Make the api call
             const response = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${inputLoc}&appid=${process.env.LOC_API_KEY}`
+                `https://api.openweathermap.org/data/2.5/weather?q=${inputLoc}&appid=226bc60742d4286f0a7f23177d6daded`
             );
                  const data = response.data;
             setApidata(data);
             
-            console.log( "weather data:",Apidata.main.temp);
+            console.log( "weather data:",Apidata);
             
            } catch (error) {
             setError("City not found. Please enter a valid city name.");
             setShowGif(true);
-            setTimeout(() => setShowGif(false), 3000);
+            setTimeout(() => setShowGif(false), 5250);
             console.log('Error fetching weather data:', error);
         }
            finally {
@@ -57,7 +57,6 @@ const checkValidity = (name) =>{
 
     }
 
-    
     return (
         <div>
              <div className="background">
@@ -67,17 +66,37 @@ const checkValidity = (name) =>{
               {loading && <p className="loading" >Loading...</p>}
             
             {error && <p className="error-message" >{error} </p>}
- 
+
+            {showGif && 
+                <div className="gif-container">
+                <img 
+                    height={200} 
+                    width={350} 
+                    src="https://i.pinimg.com/originals/96/3f/2f/963f2ffa6f7df91ab4ae0fb4f3ee2cf9.gif" 
+                    alt="brahmmi" 
+                />
+                </div>        
+                 }
+
+                  {!showGif && Apidata !== null &&
+                <div className="gif-container">
+                <img 
+                    height={150} 
+                    width={250} 
+                    src="https://media1.tenor.com/m/gNKymZ194D8AAAAd/brahmi-king.gif" 
+                    alt="brahmmi" 
+                />
+                </div>        
+                 }
+
+         
            {Apidata &&  !loading && !error &&  (
             <div className="weather-info">
                  <p>Temperature: <span>{(Apidata.main.temp - 273.15)}'C</span></p>
-                <p>Humidity: <span>{Apidata.main.humidity}%% </span></p>
+                     <p>Humidity: <span>{Apidata.main.humidity}%% </span></p>
                  <p>Wind Speed: <span>{(Apidata.wind.speed* (5/18))}km/h</span></p>
                 <p>Brief Weather Description: <span>The weather has been in the condition of {Apidata.weather[0].description}</span></p>
-             {showGif && 
-                    <img height= {100} width={200} src="https://i.pinimg.com/originals/96/3f/2f/963f2ffa6f7df91ab4ae0fb4f3ee2cf9.gif" alt="brahmmi" ></img>
-                }
-               
+            
             </div>
            )} 
            {/* <p></p>
