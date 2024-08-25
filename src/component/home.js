975410b5060e985b2cc7c7d974a33eb7b36a169d
rajcodes168h <>
2024-08-25 17:55:08 +0530

    full working website

--- src/component/home.js
+++ src/component/home.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import './home.css';
-import History from "./history"; 
 
 const Home = () => {
     const [inputLoc, setInputLoc] = useState("");
